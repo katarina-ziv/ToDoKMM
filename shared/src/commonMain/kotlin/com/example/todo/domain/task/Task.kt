2d161ba40c5c7779a6318216d18d3761d41f5c0d
katarina <katarina@kod.io>
2024-03-07 14:15:33 +0100

    Minor change in Task data class

--- shared/src/commonMain/kotlin/com/example/todo/domain/task/Task.kt
+++ shared/src/commonMain/kotlin/com/example/todo/domain/task/Task.kt
@@ -3,10 +3,10 @@ package com.example.todo.domain.task
 import kotlinx.datetime.LocalDateTime
 
 data class Task(
-    val id : Long?,
-    val title : String,
-    val content : String,
-    val isDone : Boolean,
-    val colorHex : Long,
-    val created : LocalDateTime
+    val id: Long?,
+    val title: String,
+    val content: String,
+    val isDone: Boolean,
+    val colorHex: Long,
+    val created: LocalDateTime
 )
